fe46376f14b0972a64af28425c91cc553abdc836
77yu77 <17876315884@163.com>
2023-12-13 13:49:48 +0800

    feat:add more NIC message for exporter

--- data.go
+++ data.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"os/exec"
@@ -11,167 +10,166 @@ import (
 )
 
 type Metrics struct {
-	Name string
 }
 
-type LinkPair struct {
-	Pod1       string
-	Pod2       string
-	Rbandwidth string
-	Tbandwidth string
-	Laterncy   string
-	RBFR       string
-	TBFR       string
+type NICMessage struct {
+	receive_rate         int
+	receive_packet_rate  int
+	receive_errors_rate  int
+	receive_drop_rate    int
+	transmit_rate        int
+	transmit_packet_rate int
+	transmit_errors_rate int
+	transmit_drop_rate   int
 }
 
 const (
-	vNICSymbol = "10.233"
-	LinkSymbol = "link"
-	Test       = "eth0"
-	SrcSymbol  = "src"
+	vNICSymbol                    = "Link encap"
+	receiveRateName               = "ReceiveRate"
+	receivePacketRateName         = "ReceivePacketRate"
+	receiveErrorsRateName         = "ReceiveErrorsRate"
+	receiveDropRateName           = "ReceiveDropRate"
+	transmitRateName              = "transmitRate"
+	transmitPacketRateName        = "transmitPacketRate"
+	transmitErrorsRateName        = "transmitErrorsRate"
+	transmitDropRateName          = "transmitDropRate"
+	receiveRateDescription        = "The receive data rate(bytes per second) of pod NIC"
+	receivePacketRateDescription  = "The receive packets rate(packet num per second) of pod NIC"
+	receiveErrorsRateDescription  = "The receive errors rate(errors num per second) of pod NIC"
+	receiveDropRateDescription    = "The receive drop packets rate(drop packets num per second) of pod NIC"
+	transmitRateDescription       = "The transmit data rate(bytes per second) of pod NIC"
+	transmitPacketRateDescription = "The transmit packet rate(packet num per second) of pod NIC"
+	transmitErrorsRateDescription = "The transmit errors rate(errors num per second) of pod NIC"
+	transmitDropRateDescription   = "The transmit drop packets rate(drop packets num per second) of pod NIC"
 )
 
+func GeneratePromData(name string, NIC string, data int) string {
+	result := fmt.Sprintf("%s{NICName=\"%s\"} %d\n", name, NIC, data)
+	return result
+}
+
+func GenerateDescribeMessage(name string, description string) string {
+	result := fmt.Sprintf("# HELP %s %s\n# TYPE %s counter\n", name, description, name)
+	return result
+}
+
 func (m Metrics) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	name := "LinkStatus"
-	linkTypes := []string{}
-	linkTypes = append(linkTypes, "networkName", "star1", "star2", "Rbandwidth", "Tbandwidth", "laterncy", "RBFR", "TBFR")
-	metrics := GetTopology()
-	result := fmt.Sprintf("# HELP %s data structure\n# TYPE %s counter\n", m.Name, m.Name)
-	fmt.Fprint(w, result)
-	for _, metric := range metrics {
-		data := []string{}
-		data = append(data, m.Name, metric.Pod1, metric.Pod2, metric.Rbandwidth, metric.Tbandwidth, metric.Laterncy, metric.RBFR, metric.TBFR)
-		if s, err := GeneratePromData(name, linkTypes, data); err == nil {
-			fmt.Fprint(w, s)
+	// get NIC names of pod
+	cmd := exec.Command("sh", "-c", "ifconfig")
+	stdout, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("run ifconfig cmd error:%s", err.Error())
+		return
+	}
+	outLines := strings.Split(string(stdout), "\n")
+	NIC_names := []string{}
+	for _, line := range outLines {
+		if len(line) != 0 && strings.Contains(line, vNICSymbol) {
+			words := strings.Fields(line)
+			NIC_names = append(NIC_names, words[0])
 		}
+	}
+
+	// get all NIC message
+	NIC_messages := make(map[string]NICMessage, 0)
+	for _, NIC_name := range NIC_names {
+		NIC_messages[NIC_name] = GetNICMessage(NIC_name)
+	}
+
+	// send NIC message
+	// receive data rate
+	fmt.Fprint(w, GenerateDescribeMessage(receiveRateName, receiveRateDescription))
+	for _, NIC_name := range NIC_names {
+		fmt.Fprint(w, GeneratePromData(receiveRateName, NIC_name, NIC_messages[NIC_name].receive_rate))
+	}
+
+	// receive packet rate
+	fmt.Fprint(w, GenerateDescribeMessage(receivePacketRateName, receivePacketRateDescription))
+	for _, NIC_name := range NIC_names {
+		fmt.Fprint(w, GeneratePromData(receivePacketRateName, NIC_name, NIC_messages[NIC_name].receive_packet_rate))
+	}
+
+	// receive error rate
+	fmt.Fprint(w, GenerateDescribeMessage(receiveErrorsRateName, receiveErrorsRateDescription))
+	for _, NIC_name := range NIC_names {
+		fmt.Fprint(w, GeneratePromData(receiveErrorsRateName, NIC_name, NIC_messages[NIC_name].receive_errors_rate))
+	}
+
+	// receive drop packet rate
+	fmt.Fprint(w, GenerateDescribeMessage(receiveDropRateName, receiveDropRateDescription))
+	for _, NIC_name := range NIC_names {
+		fmt.Fprint(w, GeneratePromData(receiveDropRateName, NIC_name, NIC_messages[NIC_name].receive_drop_rate))
+	}
+
+	// transmit data rate
+	fmt.Fprint(w, GenerateDescribeMessage(transmitRateName, transmitRateDescription))
+	for _, NIC_name := range NIC_names {
+		fmt.Fprint(w, GeneratePromData(transmitRateName, NIC_name, NIC_messages[NIC_name].transmit_rate))
+	}
+
+	// transmit packet rate
+	fmt.Fprint(w, GenerateDescribeMessage(transmitPacketRateName, transmitPacketRateDescription))
+	for _, NIC_name := range NIC_names {
+		fmt.Fprint(w, GeneratePromData(transmitPacketRateName, NIC_name, NIC_messages[NIC_name].transmit_packet_rate))
+	}
+
+	// transmit errors rate
+	fmt.Fprint(w, GenerateDescribeMessage(transmitErrorsRateName, transmitErrorsRateDescription))
+	for _, NIC_name := range NIC_names {
+		fmt.Fprint(w, GeneratePromData(transmitErrorsRateName, NIC_name, NIC_messages[NIC_name].transmit_errors_rate))
+	}
+
+	// transmit drop packet rate
+	fmt.Fprint(w, GenerateDescribeMessage(transmitDropRateName, transmitDropRateDescription))
+	for _, NIC_name := range NIC_names {
+		fmt.Fprint(w, GeneratePromData(transmitDropRateName, NIC_name, NIC_messages[NIC_name].transmit_drop_rate))
 	}
 
 }
 
-func GetNICTraffic(NIC string) (string, string, string, string) {
-	// cmd := exec.Command("sh", "-c", fmt.Sprintf("/flow/data.sh %s", NIC))
-	cmd := exec.Command("sh", "-c", fmt.Sprintf("cat /proc/net/dev | grep %s | sed 's/:/ /g' | awk '{print $2}'", NIC))
+func GetNICMessage(NIC string) NICMessage {
+	cmd := exec.Command("sh", "-c", fmt.Sprintf("cat /proc/net/dev | grep %s", NIC))
 	stdout, _ := cmd.CombinedOutput()
-	receive_bytes_pre, _ := strconv.Atoi(strings.Fields(string(stdout))[0])
-
-	cmd = exec.Command("sh", "-c", fmt.Sprintf("cat /proc/net/dev | grep %s | sed 's/:/ /g' | awk '{print $4}'", NIC))
-	stdout, _ = cmd.CombinedOutput()
-	receive_errors_bytes_pre, _ := strconv.Atoi(strings.Fields(string(stdout))[0])
-
-	cmd = exec.Command("sh", "-c", fmt.Sprintf("cat /proc/net/dev | grep %s | sed 's/:/ /g' | awk '{print $10}'", NIC))
-	stdout, _ = cmd.CombinedOutput()
-	transmit_bytes_pre, _ := strconv.Atoi(strings.Fields(string(stdout))[0])
-
-	cmd = exec.Command("sh", "-c", fmt.Sprintf("cat /proc/net/dev | grep %s | sed 's/:/ /g' | awk '{print $12}'", NIC))
-	stdout, _ = cmd.CombinedOutput()
-	transmit_errors_bytes_pre, _ := strconv.Atoi(strings.Fields(string(stdout))[0])
+	message_pre := strings.Fields(string(stdout))
+	receive_bytes_pre, _ := strconv.Atoi(message_pre[1])
+	receive_packet_pre, _ := strconv.Atoi(message_pre[2])
+	receive_errors_pre, _ := strconv.Atoi(message_pre[3])
+	receive_drop_pre, _ := strconv.Atoi(message_pre[4])
+	transmit_bytes_pre, _ := strconv.Atoi(message_pre[9])
+	transmit_packet_pre, _ := strconv.Atoi(message_pre[10])
+	transmit_errors_pre, _ := strconv.Atoi(message_pre[11])
+	transmit_drop_pre, _ := strconv.Atoi(message_pre[12])
 	time.Sleep(time.Second)
 
-	cmd = exec.Command("sh", "-c", fmt.Sprintf("cat /proc/net/dev | grep %s | sed 's/:/ /g' | awk '{print $2}'", NIC))
+	cmd = exec.Command("sh", "-c", fmt.Sprintf("cat /proc/net/dev | grep %s", NIC))
 	stdout, _ = cmd.CombinedOutput()
-	receive_bytes, _ := strconv.Atoi(strings.Fields(string(stdout))[0])
-
-	cmd = exec.Command("sh", "-c", fmt.Sprintf("cat /proc/net/dev | grep %s | sed 's/:/ /g' | awk '{print $4}'", NIC))
-	stdout, _ = cmd.CombinedOutput()
-	receive_errors_bytes, _ := strconv.Atoi(strings.Fields(string(stdout))[0])
-
-	cmd = exec.Command("sh", "-c", fmt.Sprintf("cat /proc/net/dev | grep %s | sed 's/:/ /g' | awk '{print $10}'", NIC))
-	stdout, _ = cmd.CombinedOutput()
-	transmit_bytes, _ := strconv.Atoi(strings.Fields(string(stdout))[0])
-
-	cmd = exec.Command("sh", "-c", fmt.Sprintf("cat /proc/net/dev | grep %s | sed 's/:/ /g' | awk '{print $12}'", NIC))
-	stdout, _ = cmd.CombinedOutput()
-	transmit_errors_bytes, _ := strconv.Atoi(strings.Fields(string(stdout))[0])
-	print(receive_bytes, " ", receive_bytes_pre, " ", transmit_bytes, " ", transmit_bytes_pre, "\n")
-	receive_errors_percent := 0
-	transmit_errors_percent := 0
-	receive_rate := (receive_bytes - receive_bytes_pre) / 1000
-	transmit_rate := (transmit_bytes - transmit_bytes_pre) / 1000
-	if receive_rate != 0 {
-		receive_errors_percent = (receive_errors_bytes - receive_errors_bytes_pre) / receive_rate
+	message := strings.Fields(string(stdout))
+	receive_bytes, _ := strconv.Atoi(message[1])
+	receive_packet, _ := strconv.Atoi(message[2])
+	receive_errors, _ := strconv.Atoi(message[3])
+	receive_drop, _ := strconv.Atoi(message[4])
+	transmit_bytes, _ := strconv.Atoi(message[9])
+	transmit_packet, _ := strconv.Atoi(message[10])
+	transmit_errors, _ := strconv.Atoi(message[11])
+	transmit_drop, _ := strconv.Atoi(message[12])
+	NIC_message := NICMessage{
+		receive_rate:         receive_bytes - receive_bytes_pre,
+		receive_packet_rate:  receive_packet - receive_packet_pre,
+		receive_errors_rate:  receive_errors - receive_errors_pre,
+		receive_drop_rate:    receive_drop - receive_drop_pre,
+		transmit_rate:        transmit_bytes - transmit_bytes_pre,
+		transmit_packet_rate: transmit_packet - transmit_packet_pre,
+		transmit_errors_rate: transmit_errors - transmit_errors_pre,
+		transmit_drop_rate:   transmit_drop - transmit_drop_pre,
 	}
-	if transmit_rate != 0 {
-		transmit_errors_percent = (transmit_errors_bytes - transmit_errors_bytes_pre) / transmit_rate
-	}
-	r_rate := strconv.Itoa(receive_rate) + "KB"
-	t_rate := strconv.Itoa(transmit_rate) + "KB"
-	r_e_rate := strconv.Itoa(receive_errors_percent) + "%"
-	t_e_rate := strconv.Itoa(transmit_errors_percent) + "%"
-	return r_rate, t_rate, r_e_rate, t_e_rate
+
+	return NIC_message
 }
 
-func GetLinkLaterncy(target string) string {
-	cmd := exec.Command("sh", "-c", fmt.Sprintf("ping -c 1 -i 0.2 %s", target))
-	stdout, _ := cmd.CombinedOutput()
-	outStr := string(stdout)
-	print("\n")
-	print(outStr)
-	outLines := strings.Split(outStr, "\n")
-	var laterncy string
-	for _, line := range outLines {
-		if strings.Contains(line, "round-trip") {
-			words := strings.Split(line, "/")
-			laterncy = words[3] + "ms"
-		}
-	}
-	if laterncy == "" {
-		return "-"
-	}
-	return laterncy
-}
-
-func GetTopology() []LinkPair {
-	cmd := exec.Command("sh", "-c", "echo $PODNAME")
-	stdout, _ := cmd.CombinedOutput()
-	podName := string(stdout)
-	print(podName)
-	cmd = exec.Command("sh", "-c", "ip route")
-	stdout, _ = cmd.CombinedOutput()
-	outStr := string(stdout)
-	outLines := strings.Split(outStr, "\n")
-	linkPairs := make([]LinkPair, 0)
-	for _, line := range outLines {
-		if len(line) != 0 {
-			if strings.Contains(line, LinkSymbol) && strings.Contains(line, SrcSymbol) {
-				words := strings.Fields(line)
-				if words[2] != Test {
-					r_rate, t_rate, r_e_rate, t_e_rate := GetNICTraffic(words[2])
-					print(r_rate, " ", t_rate, " ", r_e_rate, " ", t_e_rate, "\n")
-					linkPairs = append(linkPairs, LinkPair{Pod1: podName, Pod2: words[2], Rbandwidth: r_rate, Tbandwidth: t_rate, Laterncy: GetLinkLaterncy(words[6]), RBFR: r_e_rate, TBFR: t_e_rate})
-				}
-			}
-		}
-	}
-	return linkPairs
-}
-
-//	func (m Metrics) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-//		name := "StarStatus"
-//		types := []string{}
-//		types = append(types, "name", "timestamp", "CPU", "memory", "disk", "status")
-//		data := []string{}
-//		data = append(data, "node1", fmt.Sprint(time.Now().Unix()), "0.01%", "100MB", "200MB", "up")
-//		if s, err := GeneratePromData(name, types, data); err == nil {
-//			fmt.Fprint(w, s)
-//		}
-//	}
 func main() {
-	metric := Metrics{Name: "Network1"}
+	metric := Metrics{}
 	http.Handle("/metrics", metric)
 	http.ListenAndServe(":2112", nil)
-}
-
-func GeneratePromData(name string, types []string, datas []string) (string, error) {
-	var result string
-	if len(types) != len(datas) {
-		return result, errors.New("lens of types is difference from lens of datas for prometheus type")
-	}
-	result += name + "{"
-	for index := range types {
-		result += fmt.Sprintf("%s=\"%s\",", types[index], datas[index])
-	}
-	result = result[:len(result)-1] + "} 1\n"
-	return result, nil
 }
 
 // CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o pod_NIC_exporter data.go
